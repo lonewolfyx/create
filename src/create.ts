9a85b47f6ad63050a5e9c252008fddfce30086e4
lonewolfyx <877085893@qq.com>
2025-12-24 16:33:55 +0800

    feat: add git initialization and improved template download hints

--- src/create.ts
+++ src/create.ts
@@ -2,21 +2,28 @@ import type { Context } from '@/types.ts'
 import { spinner } from '@clack/prompts'
 import consola from 'consola'
 import { downloadTemplate } from 'giget'
+import { x } from 'tinyexec'
 
 export const create = async (config: Context): Promise<void> => {
     // start creating
     consola.info(`Creating project in ${config.projectPath}...`)
 
     const s = spinner()
-    s.start('Template Download...')
+    s.start('Template download...')
     await downloadTemplate(config.template, {
         dir: config.projectPath,
         force: true,
     })
+    s.stop('Template download done.')
 
-    s.stop('Create project success')
+    await x('git', ['init'], {
+        nodeOptions: {
+            stdio: ['ignore', 'ignore', 'inherit'],
+            cwd: config.projectPath,
+        },
+    })
     consola.success('Git initialized.')
-    // TODO 进入项目目录进行 Git 初始化
+
     // TODO 获取项目目录文件所有文件内容含有的关键词进行替换
     // TODO 安装依赖
     // TODO 提示创建完毕信息
