de9fb8adb6a0775a03927ee2c53b6fb5b1e6e673
lonewolfyx <877085893@qq.com>
2025-12-24 17:02:43 +0800

    feat: add dependency installation and improved project creation success prompts

--- src/create.ts
+++ src/create.ts
@@ -2,6 +2,8 @@ import type { Context } from '@/types.ts'
 import { spinner } from '@clack/prompts'
 import consola from 'consola'
 import { downloadTemplate } from 'giget'
+import { green } from 'picocolors'
+import { x } from 'tinyexec'
 import { git } from '@/git.ts'
 
 export const create = async (config: Context): Promise<void> => {
@@ -20,6 +22,16 @@ export const create = async (config: Context): Promise<void> => {
 
     // TODO 获取项目目录文件所有文件内容含有的关键词进行替换
     // TODO 安装依赖
-    consola.success('Done. Creating success')
+
+    consola.info('Installation in progress... ☕')
+    await x('npx', ['-y', '@antfu/ni'], {
+        nodeOptions: {
+            cwd: config.projectPath,
+            stdio: 'inherit',
+            shell: true,
+        },
+    })
+
+    consola.success(`🚀  Successfully created project ${green(config.name)}\n`)
     // TODO 检测是否有可升级的依赖包，若有 拼写相关可升级的依赖包 npm outdated && pnpm i
 }
